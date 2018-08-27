f08420418cbf93bd6a78f0212fb63e8da0356321
elvz <djelvz@gmail.com>
2018-08-27 06:40:26 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function plus() {
+  var num1, num2, result;
+  num1 = document.getElementById("n1").value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById("n2").value;
+  num2 = parseInt(num2);
+  result = num1 + num2;
+  document.getElementById("out").innerHTML = result;
+}
+function subt() {
+  var num1, num2, result;
+  num1 = document.getElementById("n1").value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById("n2").value;
+  num2 = parseInt(num2);
+  result = num1 - num2;
+  document.getElementById("out").innerHTML = result;
+}
+function mult() {
+  var num1, num2, result;
+  num1 = document.getElementById("n1").value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById("n2").value;
+  num2 = parseInt(num2);
+  result = num1 * num2;
+  document.getElementById("out").innerHTML = result;
+}
+function div() {
+  var num1, num2, result;
+  num1 = document.getElementById("n1").value;
+  num1 = parseInt(num1);
+  num2 = document.getElementById("n2").value;
+  num2 = parseInt(num2);
+  result = num1 / num2;
+  document.getElementById("out").innerHTML = result;
+}
